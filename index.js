e80d4b06ef2f588cee4a842a9d376785e4a70bcf
nh <wlsgh59391@naver.com>
2023-08-18 17:57:52 +0900

    사이드 캐러셀 추가함 , 나머지 박스 위치조정 해야함

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const prevButton = document.querySelector('.prev');
-const nextButton = document.querySelector('.next');
-const carousel = document.querySelector('.carousel');
-
 let index = 0;
 
-prevButton.addEventListener('click', () => {
+const leftButton = document.querySelector('.left');
+const rightButton = document.querySelector('.right');
+const carousel = document.querySelector('.carousel2');
+
+leftButton.addEventListener('click', () => {
   if (index === 0) return;
   index -= 1;
   
   carousel.style.transform = `translate3d(-${600 * index}px, 0, 0)`;
 });
 
-nextButton.addEventListener('click', () => {
+rightButton.addEventListener('click', () => {
   if (index === 3) return;
   index += 1;
   
@@ -19,24 +19,23 @@ nextButton.addEventListener('click', () => {
 });
 
 
+let value = 0;
+
 var emblaNode = document.querySelector(".embla");
 var options = { loop: false };
 var embla = EmblaCarousel(emblaNode, options);
 
-function setDisplayAppreance(self){
+function setDisplayAppreance(){
   var body = document.querySelector('body'); 
-  var btn = document.getElementById('btnDisplayMode'); 
-  if(self.value === '다크모드로 전환'){  //다크모드로 전환 버튼을 누른 경우
+
+  if(value === 0){  //다크모드로 전환 버튼을 누른 경우
     body.style.backgroundColor = 'rgb(8, 8, 59)';  //배경색 변경
-    body.style.color = '#5383e8'; //css가 적용되지 않은 기본 텍스트 색상 변경
-    btn.style.backgroundColor = "yellow"; //버튼의 배경색 변경
-    btn.style.color = "rgb(8, 8, 59)"; //버튼의 텍스트 색상 변경
-    self.value = '라이트모드로 전환';
+    // <i id="icon" class="fa-regular fa-sun"></i></i>
+    value = 1;
   } else { //라이트모드로 전환 버튼을 누른 경우
     body.style.backgroundColor = '#5383e8';
     body.style.color = 'darkblue';
-    btn.style.backgroundColor = "rgb(8, 8, 59)";
-    btn.style.color = "#5383e8";
-    self.value = '다크모드로 전환';
+    value = 0;
+    
   }
 }
